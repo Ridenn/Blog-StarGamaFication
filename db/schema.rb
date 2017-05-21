106c1e75746005e072e337cf979f94e8706dcf8e
Antonio Schiavon <aschiavon91@gmail.com>
2017-05-21 18:29:02 -0300

    Content model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521153912) do
+ActiveRecord::Schema.define(version: 20170521212744) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 20170521153912) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
+
+  create_table "contents", force: :cascade do |t|
+    t.string   "field"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "leads", force: :cascade do |t|
