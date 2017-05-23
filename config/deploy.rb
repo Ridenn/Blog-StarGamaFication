478b13ca127748e44629af884dd306050e251da2
Antonio Schiavon <aschiavon91@gmail.com>
2017-05-23 20:32:58 -0300

    Deploy options changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,11 +13,11 @@ set :stage,           :production
 set :deploy_via,      :remote_cache
 # Repare nesse caminho, eh o mesmo onde ficara o banco de dados da produção
 set :deploy_to, '/home/deploy/apps/blog'
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_access_log, "#{release_path}/log/puma.error.log"
+# set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
