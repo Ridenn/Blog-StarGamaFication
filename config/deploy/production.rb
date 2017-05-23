2f87f0beaa3773c40f214fc28ed828b32f219bf9
Antonio Schiavon <aschiavon91@gmail.com>
2017-05-23 19:01:39 -0300

    Changed deploy options

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '45.33.100.39', user: 'blog', roles: %w{app db web} 
+server '45.33.100.39', user: 'deploy', roles: %w{app db web} 
 
 # role-based syntax
 # ==================
