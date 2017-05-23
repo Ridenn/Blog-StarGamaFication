9a84fb96aed5131cee154e0fe5122ceaf5fa6dcc
Antonio Schiavon <aschiavon91@gmail.com>
2017-05-23 19:44:42 -0300

    Changed deploy options

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,28 @@
-# Rodar com pty e sem sudo
-set :pty, true
-set :use_sudo, "false"
-
 # Nome da aplicação
 set :application, 'blog'
 
 # Repositório para baixar e usar
 set :repo_url, 'git@github.com:aschiavon91/Blog-StarGamaFication.git'
 
+set :user,            'deploy'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
 # Repare nesse caminho, eh o mesmo onde ficara o banco de dados da produção
 set :deploy_to, '/home/deploy/apps/blog'
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-# Set nginx options
-#set :nginx_domains, "empreededorismoideias.com.br"
 
 # Linka arquivos e pastas
 append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/application.yml'
@@ -34,6 +44,39 @@ namespace :figaro do
       upload! 'config/application.yml', "#{shared_path}/config/application.yml"
     end
   end
+end
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
 end
 
 # Rodar antes do check deploy a task upload (logo acima)
