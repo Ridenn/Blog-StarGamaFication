f7e2c9abebafffaa7cfd25cd0b4087e102ee12fa
Antonio Schiavon <aschiavon91@gmail.com>
2017-05-25 03:12:09 -0300

    ckeditor setted to use 100%width

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,6 +1,6 @@
 CKEDITOR.editorConfig = function(config) {
-  config.language = 'en';
-  config.width = '700';
+  config.language = 'pt-BR';
+  config.width = '100%';
   config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
   config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
   config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
