70b84d2b073e9d35c11646cd42200b38f8212117
Lucas <lucaspedrosa11@hotmail.com>
2017-05-24 23:43:07 -0300

    Added form to contact page
    - Added form to contact page;
    - Added eBook to the application.
    - Added subject to the home page;
    - Changed href of envelope icon.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,12 +4,13 @@ class HomeController < ApplicationController
   end
 
   def contact
+    @lead = Lead.new
   end
 
   def thanks
   end
 
    def pdf
-    send_file Rails.root.join('private', 'ebook.pdf'), :type=>"application/pdf", :x_sendfile=>true
+    send_file Rails.root.join('private', 'Como_Mensurar_e_Otimizar.pdf'), :type=>"application/pdf", :x_sendfile=>true
   end
 end
