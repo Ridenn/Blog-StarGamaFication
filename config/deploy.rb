7167aa8fc34bbdb816f3762fc4b1f26c632d0749
Antonio Schiavon <aschiavon91@gmail.com>
2017-05-25 15:58:22 -0300

    deploy fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -73,16 +73,15 @@ namespace :deploy do
       invoke 'puma:restart'
     end
   end
+end
 
-  namespace :logs do
-    desc "tail rails logs"
-    task :tail_rails do
-      on roles(:app) do
-        execute "tail -f #{shared_path}/log/#{fetch(:rails_env)}.log"
-      end
+namespace :logs do
+  desc "tail rails logs"
+  task :tail_rails do
+    on roles(:app) do
+      execute "tail -f #{shared_path}/log/#{fetch(:rails_env)}.log"
     end
   end
-
 end
 
 
