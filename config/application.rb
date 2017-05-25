4f04c511d80d1872ae861bb3054c982dcec7286c
Antonio Schiavon <aschiavon91@gmail.com>
2017-05-25 15:11:53 -0300

    Try fix ckeditor #9

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module EmpreendedorismoIdeiasBlog
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Brasilia'
     config.active_record.default_timezone = :local
+    config.assets.precompile += %w( ckeditor/* )
   end
 end
