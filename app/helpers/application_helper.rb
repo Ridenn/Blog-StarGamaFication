db957f90ee72b417eb2c71ca1f669f165a6890a9
Antonio Schiavon <aschiavon91@gmail.com>
2017-05-26 02:01:38 -0300

    Uploader fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3649,4 +3649,5 @@ module ApplicationHelper
   def highlight_img(post)
     Nokogiri::HTML.fragment(post.body).css('img').remove_attr('style').add_class('img-responsive').collect(&:to_s)[0]
   end
+  
 end
